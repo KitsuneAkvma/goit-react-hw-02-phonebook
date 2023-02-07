f354dd84d16bcf152bc15c44eb0e767aa947992f
Mateusz Martin <106407746+KitsuneAkvma@users.noreply.github.com>
2023-02-07 05:53:33 +0100

    removed placeholder contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,16 +8,7 @@ import { ContactsList } from './ContactsList/ContactsList';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      { id: 'id-5', name: 'Cat Chad', number: '227-91-26' },
-      { id: 'id-6', name: 'Doogie Boomer', number: '227-91-26' },
-      { id: 'id-7', name: 'Annie Drown', number: '227-91-26' },
-      { id: 'id-8', name: 'Eden Simpson', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
