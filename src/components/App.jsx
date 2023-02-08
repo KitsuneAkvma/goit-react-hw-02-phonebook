5b97f483d148fc56571a9468727059042d5767b5
Mateusz Martin <mateusz.martinn@gmail.com>
2023-02-08 21:32:29 +0100

    Removed unused Code
    close #4 Faktycznie! Dzięki za trafną uwagę, chyba się pogubiłem w środku i zapomniałem co chciałem osiągnąć :
    close #5 Uznałem, że zostawię jakbyś chciał testować search ;) Usunięte już.
    close #8 Cała metoda była w sumie nie potrzebna

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -47,9 +47,6 @@ export class App extends Component {
     const input = e.currentTarget;
 
     this.setState({ filter: input.value });
-    this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(input.value.toLowerCase())
-    );
   };
 
   handleRemoveContact = e => {
@@ -83,13 +80,3 @@ export class App extends Component {
 }
 
 export default App;
-
-//Placeholder contacts
-// { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-// { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-// { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-// { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// { id: 'id-5', name: 'Cat Chad', number: '227-91-26' },
-// { id: 'id-6', name: 'Doogie Boomer', number: '227-91-26' },
-// { id: 'id-7', name: 'Annie Drown', number: '227-91-26' },
-// { id: 'id-8', name: 'Eden Simpson', number: '227-91-26' },
